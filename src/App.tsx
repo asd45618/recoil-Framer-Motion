b3612aee37eae0019fbc2884fbfa705cbc531a49
asd45618 <asd45618@naver.com>
2024-10-10 21:37:47 +0900

    recoil-Framer-Motion

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const myVars = {
+  start: { scale: 0 },
+  end: {
+    scale: 1,
+    rotateZ: '360deg',
+    transition: { type: 'spring', delay: 0.5 },
+  },
+};
+
+function App() {
+  return (
+    <Wrapper>
+      <Box variants={myVars} initial='start' animate='end' />
+    </Wrapper>
+  );
+}
+
+export default App;
